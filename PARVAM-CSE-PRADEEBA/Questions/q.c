e7283d2dfd0bf1fe8a113df439bc2a85a739346a
pradeeba26 <pradeeba2606@gmail.com>
2024-11-07 09:28:09 +0530

    1st commit

---
+++ PARVAM-CSE-PRADEEBA/Questions/q.c
@@ -0,0 +1,10 @@
+Write a C program to find the Factorial of a number
+Factorial is a sequence of a number where we multiply by all previous numbers. 
+
+factorial of n (n!) = 1 * 2 * 3 * 4....n
+
+
+Note : 0! = 1 and 1! = 1
+
+Example : 
+5! = 1 * 2 * 3 * 4 * 5 = 120
