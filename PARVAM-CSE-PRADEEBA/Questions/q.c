4e7a12b0ec53d7779689d5295c78add04139375f
pradeeba26 <109218877+pradeeba26@users.noreply.github.com>
2024-11-07 16:01:17 +0530

    Update q.c

--- PARVAM-CSE-PRADEEBA/Questions/q.c
+++ PARVAM-CSE-PRADEEBA/Questions/q.c
@@ -1,7 +1,10 @@
-// Factors of a Number in C
-// Here, in this page, we will discuss the program to find factors of a number in C programming language. We will discuss different methods to find the factors of the given number.
+// Write a C Program to check if number is Harshad number or not
+// In mathematics, a Harshad number is a number that is divisible by the sum of its digits.
 
-// Example :
+// Ex- Number is 21
 
-// Input : 10
-// Output : 1, 2, 5, 10
+// it is divisible by its own sum (1+2) of its digit(2,1)
+
+// So it is Harshad's Number
+
+// Some other Harshad's Number are 156,54,120 etc
