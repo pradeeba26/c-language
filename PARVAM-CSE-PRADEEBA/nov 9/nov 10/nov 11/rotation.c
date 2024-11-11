f8b9a987aaa23cdde223b94d23b274786352a641
pradeeba26 <pradeeba2606@gmail.com>
2024-11-11 16:21:14 +0530

    1st commit

--- PARVAM-CSE-PRADEEBA/nov 9/nov 10/nov 11/rotation.c
+++ PARVAM-CSE-PRADEEBA/nov 9/nov 10/nov 11/rotation.c
@@ -6,5 +6,18 @@ int main(){
     for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
-    int temp=arr[n-1];
+    int k;
+    scanf("%d",&k);
+    for(int j=0;j<k;j++){
+        int temp=arr[n-1];
+        for(int i=n-1;i>0;i--){
+            arr[i]=arr[i-1];
+        }
+        arr[0]=temp;
+    }
     
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+    
+}
